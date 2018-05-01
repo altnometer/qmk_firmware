c60dddfce9abf5dbe4e12aac5f7c16cb50125dbb
sam <sam@redmoo.info>
2018-05-01 13:08:08 +0300

    Changed M_SFT_D to MLSFT_D.

--- keyboards/dz60/keymaps/altnometer/keymap.c
+++ keyboards/dz60/keymaps/altnometer/keymap.c
@@ -23,7 +23,7 @@
 #define  L_FN3_Z LT(_FN3, KC_Z)
 
 
-#define  M_SFT_D MT(MOD_LSFT, KC_D)
+#define  MLSFT_D MT(MOD_LSFT, KC_D)
 
 #define  KC_RGHT KC_RIGHT
 #define  KC_PGDN KC_PGDOWN
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FN0] = LAYOUT(
 		F(0)   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , XXXXXXX, KC_BSPC,
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
-		MO(2)  , L_FN1_A, L_FN2_S, M_SFT_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , L_FN1  , KC_QUOT, KC_ENT ,
+		MO(2)  , L_FN1_A, L_FN2_S, MLSFT_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , L_FN1  , KC_QUOT, KC_ENT ,
 		KC_LSFT, XXXXXXX, L_FN3_Z, KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, XXXXXXX,
-		KC_LALT, KC_LCTL, KC_LGUI, KC_SPC , KC_SPC , KC_SPC , KC_RCTL, KC_RALT, M(2)   , MO(1)  , XXXXXXX),
+		KC_LALT, KC_LCTL, KC_LGUI, KC_SPC , KC_SPC , KC_SPC , KC_RCTL, KC_RALT, M(3)   , MO(1)  , XXXXXXX),
 
 	[_FN1] = LAYOUT(
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL,
