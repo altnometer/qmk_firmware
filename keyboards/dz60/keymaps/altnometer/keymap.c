486e14e784ae9ea370e23bd75c6f0f66d109ae8c
sam <sam@redmoo.info>
2018-05-01 09:31:30 +0300

    Drafted layer 1 right hand numerals

--- keyboards/dz60/keymaps/altnometer/keymap.c
+++ keyboards/dz60/keymaps/altnometer/keymap.c
@@ -39,17 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_FN1] = LAYOUT(
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL,
-		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , _______,
-		_______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP , KC_RIGHT, KC_LBRC, KC_RBRC, KC_BSPC,
-		_______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, _______, _______,
-		_______, _______, _______, KC_ENT , KC_ENT , KC_ENT , KC_ENT , _______, _______, _______, _______),
-
-	/* LAYOUT( */
-	/* 	KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL, */
-	/* 	KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, */
-	/* 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, */
-	/* 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC, BL_TOGG, BL_INC, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, */
-	/* 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS), */
+		KC_GRV , _______, _______, _______, _______, _______, _______, KC_DOT , KC_8   , KC_9   , KC_7   , _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, KC_0   , KC_1   , KC_2   , KC_3   , _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, KC_4   , KC_5   , KC_6   , _______, _______, _______,
+		_______, _______, _______, KC_BSPC, KC_BSPC, KC_BSPC, _______ , _______, _______, _______, _______),
 
 	[_FN2] = LAYOUT(
 		_______, M(1)   , M(2)   , M(3)   , M(4)   , M(5)   , M(6)   , M(7)   , M(8)   , M(9)   , M(10)   , M(11) , M(12)  , _______, _______,
