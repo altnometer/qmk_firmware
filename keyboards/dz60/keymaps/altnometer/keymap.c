937e1eb69bac4c90a1fbb3115bc85aa923f03513
sam <sam@redmoo.info>
2018-05-01 13:28:51 +0300

    Added KC_LPRN and KC_RPRN to numerical layout.
    Moved left home row symbols to the right to fit new symbol.
    Moved KC_PLUS to a new place.

--- keyboards/dz60/keymaps/altnometer/keymap.c
+++ keyboards/dz60/keymaps/altnometer/keymap.c
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_FN1] = LAYOUT(
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL,
-		KC_GRV , KC_PLUS, KC_UNDS, KC_MINS, KC_EQL , _______, _______, KC_DOT , KC_8   , KC_9   , KC_7   , _______, _______, _______,
-		_______, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, _______, KC_ENT , KC_1   , KC_2   , KC_3   , KC_0   , _______, _______,
+		KC_GRV , KC_LBRC, KC_UNDS, KC_MINS, KC_EQL , KC_PLUS, _______, KC_DOT , KC_8   , KC_9   , KC_7   , _______, _______, _______,
+		_______, KC_RBRC, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_ENT , KC_1   , KC_2   , KC_3   , KC_0   , _______, _______,
 		_______, XXXXXXX, KC_CIRC, KC_HASH, KC_DLR , KC_ASTR, _______, KC_COMM, KC_4   , KC_5   , KC_6   , KC_COMM, _______, _______,
 		_______, _______, _______, KC_BSPC, KC_BSPC, KC_BSPC, _______ , _______, _______, _______, _______),
 
