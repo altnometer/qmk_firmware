201ea3f870677afd25a4f11a60fa6f8d541459cf
sam <sam@redmoo.info>
2018-05-01 19:07:35 +0300

    Adjusting layout.

--- keyboards/dz60/keymaps/altnometer/keymap.c
+++ keyboards/dz60/keymaps/altnometer/keymap.c
@@ -1,9 +1,10 @@
 #include "dz60.h"
 
+/* #undef TAPPING_TERM  // how long before a tap becomes a hold */
+/* #define TAPPING_TERM 300 // default value is 200 */
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-
 #define _FN0 0
 #define _FN1 1
 #define _FN2 2
@@ -24,6 +25,7 @@
 
 
 #define  MLSFT_D MT(MOD_LSFT, KC_D)
+#define  MLCTL_L MT(MOD_LCTL, KC_L)
 
 #define  KC_RGHT KC_RIGHT
 #define  KC_PGDN KC_PGDOWN
@@ -33,16 +35,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FN0] = LAYOUT(
 		F(0)   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , XXXXXXX, KC_BSPC,
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
-		MO(2)  , L_FN1_A, L_FN2_S, MLSFT_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , L_FN1  , KC_QUOT, KC_ENT ,
+		MO(2)  , L_FN1_A, L_FN2_S, MLSFT_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , MLCTL_L, L_FN1  , KC_QUOT, KC_ENT ,
 		KC_LSFT, XXXXXXX, L_FN3_Z, KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, XXXXXXX,
-		KC_LALT, KC_LCTL, KC_LGUI, KC_SPC , KC_SPC , KC_SPC , KC_RCTL, KC_RALT, XXXXXXX, MO(3)  , XXXXXXX),
+		KC_LALT, KC_LGUI, KC_LCTL, KC_SPC , KC_SPC , KC_SPC , KC_RCTL, KC_RALT, XXXXXXX, MO(3)  , XXXXXXX),
 
 	[_FN1] = LAYOUT(
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL,
-		KC_GRV , KC_LBRC, KC_UNDS, KC_MINS, KC_EQL , KC_PLUS, _______, KC_DOT , KC_8   , KC_9   , KC_7   , _______, _______, _______,
-		_______, KC_RBRC, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_ENT , KC_1   , KC_2   , KC_3   , KC_0   , _______, _______,
-		_______, XXXXXXX, KC_CIRC, KC_HASH, KC_DLR , KC_ASTR, _______, KC_COMM, KC_4   , KC_5   , KC_6   , KC_COMM, _______, _______,
-		_______, _______, _______, KC_BSPC, KC_BSPC, KC_BSPC, _______ , _______, _______, _______, _______),
+		KC_GRV , KC_LBRC, KC_UNDS, KC_MINS, KC_EQL , KC_PLUS, _______, KC_7   , KC_8   , KC_9   , KC_DOT , _______, _______, _______,
+		_______, KC_RBRC, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, _______, KC_1   , KC_2   , KC_3   , KC_0   , _______, _______,
+		_______, XXXXXXX, KC_CIRC, KC_HASH, KC_DLR , KC_ASTR, _______, KC_ENT , KC_4   , KC_5   , KC_6   , KC_COMM, _______, _______,
+		_______, _______, _______, KC_BSPC, KC_BSPC, KC_BSPC, _______, _______, _______, _______, _______),
 
 	[_FN2] = LAYOUT(
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
