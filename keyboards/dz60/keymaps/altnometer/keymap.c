e122a32315dccd0af7d7fab6532bfc54e2abd5ba
sam <sam@redmoo.info>
2018-05-02 18:11:52 +0300

    Swapped KC_LGUI and KC_LCTL.

--- keyboards/dz60/keymaps/altnometer/keymap.c
+++ keyboards/dz60/keymaps/altnometer/keymap.c
@@ -1,7 +1,5 @@
 #include "dz60.h"
 
-/* #undef TAPPING_TERM  // how long before a tap becomes a hold */
-/* #define TAPPING_TERM 300 // default value is 200 */
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -18,11 +16,9 @@
 #define MS_BTN2 KC_MS_BTN2
 #define MS_ACCEL1 KC_MS_ACCEL1
 
-#define  L_FN2_S LT(_FN2, KC_S)
-#define  L_FN1 LT(_FN1, KC_SCLN)
-#define  L_FN1_A LT(_FN1, KC_A)
-#define  L_FN3_Z LT(_FN3, KC_Z)
-
+#define L_FN2_S LT(_FN2, KC_S)
+#define L_FN1 LT(_FN1, KC_SCLN)
+#define L_FN1_A LT(_FN1, KC_A)
 
 #define  MLSFT_D MT(MOD_LSFT, KC_D)
 #define  MLCTL_L MT(MOD_LCTL, KC_L)
@@ -36,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		F(0)   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , XXXXXXX, KC_BSPC,
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
 		MO(2)  , L_FN1_A, L_FN2_S, MLSFT_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , MLCTL_L, L_FN1  , KC_QUOT, KC_ENT ,
-		KC_LSFT, XXXXXXX, L_FN3_Z, KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, XXXXXXX,
-		KC_LALT, KC_LGUI, KC_LCTL, KC_SPC , KC_SPC , KC_SPC , KC_RCTL, KC_RALT, XXXXXXX, MO(3)  , XXXXXXX),
+		KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, XXXXXXX,
+		KC_LALT, KC_LCTL, KC_LGUI, KC_SPC , KC_SPC , KC_SPC , KC_RCTL, KC_RALT, XXXXXXX, MO(3)  , XXXXXXX),
 
 	[_FN1] = LAYOUT(
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL,
