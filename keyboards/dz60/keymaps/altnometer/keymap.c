8beb791385f9d941182d6f40b25299f199e7f789
sam <sam@redmoo.info>
2018-05-02 18:20:47 +0300

    Corrected MLCTL_L to MRCTL_L.

--- keyboards/dz60/keymaps/altnometer/keymap.c
+++ keyboards/dz60/keymaps/altnometer/keymap.c
@@ -21,7 +21,7 @@
 #define L_FN1_A LT(_FN1, KC_A)
 
 #define  MLSFT_D MT(MOD_LSFT, KC_D)
-#define  MLCTL_L MT(MOD_LCTL, KC_L)
+#define  MRCTL_L MT(MOD_RCTL, KC_L)
 
 #define  KC_RGHT KC_RIGHT
 #define  KC_PGDN KC_PGDOWN
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FN0] = LAYOUT(
 		F(0)   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , XXXXXXX, KC_BSPC,
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
-		MO(2)  , L_FN1_A, L_FN2_S, MLSFT_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , MLCTL_L, L_FN1  , KC_QUOT, KC_ENT ,
+		MO(2)  , L_FN1_A, L_FN2_S, MLSFT_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , MRCTL_L, L_FN1  , KC_QUOT, KC_ENT ,
 		KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, XXXXXXX,
 		KC_LALT, KC_LCTL, KC_LGUI, KC_SPC , KC_SPC , KC_SPC , KC_RCTL, KC_RALT, XXXXXXX, MO(3)  , XXXXXXX),
 
