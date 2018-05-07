1565c0727b7c20a712aee41b45c10ca4d4fc74e4
sam <sam@redmoo.info>
2018-05-07 17:30:13 +0300

    Changed MO(3) with a #defined command.

--- keyboards/dz60/keymaps/altnometer/keymap.c
+++ keyboards/dz60/keymaps/altnometer/keymap.c
@@ -25,6 +25,7 @@ enum layers {
 #define L_NUMSP LT(NUMER, KC_SPC)
 #define T_NUMER TT(NUMER)
 #define OS_NAV OSL(NAVIG)
+#define MO_MOUS MO(MOUSE)
 
 // Modifier Switching.
 #define  MLSFT_D MT(MOD_LSFT, KC_D)
@@ -73,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
 		OS_LGUI, KC_A   , MRCTL_S, MLSFT_D, MLALT_F, KC_G   , KC_H   , MRALT_J, MRSFT_K, MRCTL_L, KC_SCLN, KC_QUOT, KC_ENT ,
 		KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, XXXXXXX,
-		KC_LALT, OS_LGUI, OS_NAV , L_NUMSP, L_NUMSP, L_NUMSP, T_NUMER, KC_RALT, XXXXXXX, MO(3)  , XXXXXXX),
+		KC_LALT, OS_LGUI, OS_NAV , L_NUMSP, L_NUMSP, L_NUMSP, T_NUMER, KC_RALT, XXXXXXX, MO_MOUS, XXXXXXX),
 
         /* NUMSH
         * ,-----------------------------------------------------------------------------------------.
