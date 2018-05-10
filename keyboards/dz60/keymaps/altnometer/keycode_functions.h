cdf29a16d047730d685298512515219f6f966e77
sam <sam@redmoo.info>
2018-05-10 11:57:22 +0300

    Added files enabling tapdance.

---
+++ keyboards/dz60/keymaps/altnometer/keycode_functions.h
@@ -0,0 +1,17 @@
+/* credit to http://thedarnedestthing.com/colophon */
+// .......................................................... Keycode Primitives
+
+// register simple key press
+void tap_key(uint16_t keycode)
+{
+  register_code  (keycode);
+  unregister_code(keycode);
+}
+
+void shift_key(uint16_t keycode)
+{
+  register_code  (KC_LSFT);
+  tap_key        (keycode);
+  unregister_code(KC_LSFT);
+}
+
