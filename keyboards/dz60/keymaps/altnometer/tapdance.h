5a394f96b44ea88d8540a1c43b13ec9013379b0c
sam <sam@redmoo.info>
2018-05-12 06:29:15 +0300

    Added tap dance for KC_EQL. double tap is :=

--- keyboards/dz60/keymaps/altnometer/tapdance.h
+++ keyboards/dz60/keymaps/altnometer/tapdance.h
@@ -4,10 +4,11 @@
 enum tap_dance {
      _EIGHT = 0
     ,_TILD
+    ,_EQL
 };
 #define TD_EIGH TD(_EIGHT)
 #define TD_TILD TD(_TILD)
-
+#define TD_EQL TD(_EQL)
 
 // Tap Dance Definitions: ~ to ~/
 void tilde(qk_tap_dance_state_t *state, void *user_data)
@@ -33,9 +34,33 @@ void tilde_reset(qk_tap_dance_state_t *state, void *user_data)
   unregister_code(KC_LSFT);
 }
 
+// = to := as in golang.
+void equal(qk_tap_dance_state_t *state, void *user_data)
+{
+  // double tap plus down: repeating keycode
+  if (state->count > 2) {
+    register_code(KC_EQL);
+  }
+  // tap: keycode
+  else {
+    tap_key(KC_EQL);
+    // double tap: golang := symbol
+    if (state->count > 1) {
+      shift_key(KC_SCLN);
+      tap_key(KC_EQL);
+    }
+  }
+}
+
+void equal_reset(qk_tap_dance_state_t *state, void *user_data)
+{
+  unregister_code(KC_EQL);
+}
+
 // Tap Dance Definitions.
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for 8, twice for *
      [_EIGHT]  = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_ASTR)
     ,[_TILD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tilde, tilde_reset)
+    ,[_EQL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, equal, equal_reset)
 };
