0c63ca9d0833b6c509c98b696d7c9684cbc37c27
sam <sam@redmoo.info>
2018-05-24 08:09:49 +0300

    Set TAP_DANCE_ENABLE to no

--- keyboards/dz60/keymaps/altnometer/rules.mk
+++ keyboards/dz60/keymaps/altnometer/rules.mk
@@ -1,7 +1,7 @@
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-TAP_DANCE_ENABLE = yes    # raises an error when compiling.
+TAP_DANCE_ENABLE = no       # raises an error when compiling if tap_dance_actions[] isn't defined.
 
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
