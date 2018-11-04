e536a9df8cae5993c72ff1ed7f6c514dc8870a03
puppy <puppy@dog.lan>
2018-11-04 08:30:00 +0300

    Added custom lmt() to activate layer, mod or tap

--- keyboards/niu_mini/keymaps/altnometer/keycode_functions.h
+++ keyboards/niu_mini/keymaps/altnometer/keycode_functions.h
@@ -1,5 +1,6 @@
 /* credit to http://thedarnedestthing.com/colophon */
 // .......................................................... Keycode Primitives
+ /* #include <print.h> */
 
 // register simple key press
 void tap_key(uint16_t keycode)
@@ -70,6 +71,24 @@ void lt_shift(keyrecord_t *record, uint16_t keycode, uint8_t layer)
     // for shifted keycodes, hence, LT_SHIFT
     key_press(SHIFT, keycode);
     clear_mods();
+    key_timer = 0;
+  }
+}
+
+// activate L-ayer, activafe M-ode, send keycode if within T-APPING_TERM
+void lmt(keyrecord_t *record, uint8_t layer, uint16_t modifier, uint16_t keycode)
+{
+  if (record->event.pressed) {
+    key_timer = timer_read();
+    register_code(modifier);
+    layer_on(layer);
+  }
+  else {
+    unregister_code(modifier);
+    layer_off(layer);
+    if (timer_elapsed(key_timer) < TAPPING_TERM) {
+        tap_key(keycode);
+    }
     key_timer = 0;
   }
 }
