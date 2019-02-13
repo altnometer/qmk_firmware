64ccfd99527b2232add4b99cb5c5869f440e423e
puppy <puppy@dog.lan>
2019-02-13 13:28:12 +0300

    Added 'abcdef' to NUMER layer for hexadecimals

--- keyboards/niu_mini/keymaps/altnometer/keymap.c
+++ keyboards/niu_mini/keymaps/altnometer/keymap.c
@@ -158,19 +158,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* NUMER
  * ,-----------------------------------------------------------------------------------.
- * |      |  5   |  =   |  4   |      |      |      |      |   8  |   *  |   9  |      |
+ * |  d   |  5   |  =   |  4   |  x   |      |      |      |   8  |   *  |   9  |   a  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  3   |  2   |  1   | .  # |      |      |   ;  |   0  |   6  |   7  |      |
+ * |  e   |  3   |  2   |  1   | .  # |      |      |   ;  |   0  |   6  |   7  |   b  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  \   | /  ? | '  ` | ,  ! |   &  |      |      |   +  |   -  |   |  |   :  |   _  |
+ * |  f   | /  ? | '  ` | ,  ! |   &  |      |      |   +  |   -  |   |  |   :  |   c  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
  [NUMER] = LAYOUT_ortho_4x12(
-  _______, KC_5   , KC_EQL , KC_4   , _______, XXXXXXX, XXXXXXX, _______, KC_8   , KC_ASTR, KC_9   , KC_V   ,
-  KC_LALT, MALT_3 , KC_2   , MCTL_1 , MY_DOT , XXXXXXX, XXXXXXX, KC_SCLN, MCTL_0 , KC_6   , MALT_7 , KC_LALT,
-  KC_BSLS, KC_SLSH, MY_QUOT, MY_COMM, KC_AMPR, XXXXXXX, XXXXXXX, KC_PLUS, KC_MINS, KC_PIPE, KC_COLN, KC_UNDS,
+  KC_D   , KC_5   , KC_EQL , KC_4   , KC_X   , XXXXXXX, XXXXXXX, _______, KC_8   , KC_ASTR, KC_9   , KC_A   ,
+  KC_E   , MALT_3 , KC_2   , MCTL_1 , MY_DOT , XXXXXXX, XXXXXXX, KC_SCLN, MCTL_0 , KC_6   , MALT_7 , KC_B   ,
+  KC_F   , KC_SLSH, MY_QUOT, MY_COMM, KC_AMPR, XXXXXXX, XXXXXXX, KC_PLUS, KC_MINS, KC_PIPE, KC_COLN, KC_C   ,
   _______, _______, _______, _______, L_BSMSP, _______, _______, _______, _______, _______, _______, _______
 ),
   [BAREBKL] = LAYOUT_ortho_4x12(
