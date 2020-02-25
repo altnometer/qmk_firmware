42a226149f8a38ca634d6be6e8f7484dc91eebe7
sam at lf <sam\@lf>
2020-02-25 12:16:05 +0300

    handwire: try 4 col by 4 row layout

--- keyboards/altnometer/config.h
+++ keyboards/altnometer/config.h
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { B1, B3 }
-#define MATRIX_COL_PINS { B5, B4, E6 }
+#define MATRIX_ROW_PINS { B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
