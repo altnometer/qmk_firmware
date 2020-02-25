c67cda5285072b326eb3399b7224b899b1797eaf
kitty <kitty@noemail.yet>
2020-02-25 05:47:05 +0000

    altnometer: 3 col 2 row working

--- keyboards/altnometer/config.h
+++ keyboards/altnometer/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS { B1, B3 }
-#define MATRIX_COL_PINS { E6, B4, B5 }
+#define MATRIX_COL_PINS { B5, B4, E6 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
