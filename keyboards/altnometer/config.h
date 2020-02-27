b75b72fd5cdf8b5b95b69ac66305e7179257d351
sam at lf <sam\@lf>
2020-02-27 12:48:54 +0300

    handwire: try 5x4 layout

--- keyboards/altnometer/config.h
+++ keyboards/altnometer/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 4
-#define MATRIX_COLS 4
+#define MATRIX_COLS 5
 
 /*
  * Keyboard Matrix Assignments
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS { B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { B5, B4, E6, D7 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7, D4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
