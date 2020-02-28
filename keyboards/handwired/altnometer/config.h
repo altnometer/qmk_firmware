5c387a80533d41b7003def89fa2cce63f03c8f7d
sam at lf <sam\@lf>
2020-02-28 11:01:35 +0300

    handwired: set SPLIT_KEYBOARD to 'yes'

--- keyboards/handwired/altnometer/config.h
+++ keyboards/handwired/altnometer/config.h
@@ -52,6 +52,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
+// either USE_I2C or SOFT_SERIAL_PIN should be set to compile
+// if SPLIT_KEYBOARD is set to 'yes'
+#define USE_I2C
 //#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
