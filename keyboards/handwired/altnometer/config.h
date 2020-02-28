d7a1c767d1795a91b0a3da62ead5123c6ff8113b
sam at lf <sam\@lf>
2020-02-28 10:24:05 +0300

    handwired: modified config.h and rules.h
    Set the basics for transition to spit layout.
    Setting SPLIT_KEYBOARD to yes causes an error at this point.

--- keyboards/handwired/altnometer/config.h
+++ keyboards/handwired/altnometer/config.h
@@ -23,12 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    You
+#define MANUFACTURER    no_manufacture
 #define PRODUCT         altnometer
-#define DESCRIPTION     A custom keyboard
+#define DESCRIPTION     Fit the key positions to your liking in a split layout
 
 /* key matrix size */
-#define MATRIX_ROWS 4
+// rows are doubled-up
+#define MATRIX_ROWS 8
 #define MATRIX_COLS 5
 
 /*
@@ -51,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
+//#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
