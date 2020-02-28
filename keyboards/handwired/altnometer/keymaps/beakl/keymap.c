d2e71e6e51c4b465c4a2beb7ad56d9edce68f84d
sam at lf <sam\@lf>
2020-02-28 12:39:07 +0300

    handwired: add qwerty layout to beakl keymap

--- keyboards/handwired/altnometer/keymaps/beakl/keymap.c
+++ keyboards/handwired/altnometer/keymaps/beakl/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 REPLACE_WITH_YOUR_NAME
+/* Copyright 2019 altnometer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,42 +15,161 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  QMKBEST = SAFE_RANGE,
-  QMKURL
+#include "keycode_functions.h"
+
+// Define the tap-hold delay here, otherwise it defaults to 200 ms
+#define TAP_HOLD_DELAY 170
+#include "process_tap_hold.h"
+
+#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+// Layers Declarations.
+enum layers {
+     _QWERTY = 0
+    ,_BEAKL
+    ,SYMBL
+    ,NUMER
+    ,FLAYER
+    ,BAREBKL
+    ,BARESBL
+    ,NAVIG
+    ,I3WM
+    ,TMUX
+    ,MOUSE
 };
+
+enum planck_keycodes {
+   QWERTY = SAFE_RANGE
+  ,BEAKL
+  ,MY_COMM
+  ,MY_MINS
+  ,MY_DOT
+  ,MY_QUOT
+  ,BACKLT
+  ,HOMELPN
+  ,HOMERPN
+  ,HOMEDLR
+  ,I3_ESC
+  ,TMX_INS
+  /* ,HOME_AT */
+  /* ,HOME_PR */
+  ,_QK_TAP_HOLD // Has to be the last element
+};
+
+// Place this below the custom keycodes
+uint16_t QK_TAP_HOLD = _QK_TAP_HOLD;
+#define TH(n) (_QK_TAP_HOLD + n)
+
+// This is where you place you tap-hold actions
+// You can use the macros:
+// ACTION_TAP_HOLD_SHIFT(KC_TAP, KC_HOLD, KC_TAP_SHIFT, KC_HOLD_SHIFT)
+// and ACTION_TAP_HOLD(KC_TAP, KC_HOLD)
+tap_hold_action_t tap_hold_actions[] = {
+  [0] = ACTION_TAP_HOLD_SHIFT(KC_MENU, KC_HELP, KC_STOP, KC_FIND),
+  [1] = ACTION_TAP_HOLD(KC_A, KC_1),
+  [2] = ACTION_TAP_HOLD(KC_B, KC_2)
+};
+
+// Mouse Declarations.
+#define MS_LEFT KC_MS_LEFT
+#define MS_RGHT KC_MS_RIGHT
+#define MS_UP KC_MS_UP
+#define MS_DOWN KC_MS_DOWN
+#define MS_BTN1 KC_MS_BTN1
+#define MS_BTN2 KC_MS_BTN2
+#define MS_ACCEL1 KC_MS_ACCEL1
+
+// Layer Switching.
+#define L_NAVSP LT(NAVIG, KC_SPC)
+#define L_NAVBS LT(NAVIG, KC_BSPC)
+#define L_NAVTB LT(NAVIG, KC_TAB)
+
+#define L_SYM1 LT(SYMBL, KC_1)
+#define L_SYM0 LT(SYMBL, KC_0)
+#define L_SYMS LT(SYMBL, KC_S)
+#define L_SYMA LT(SYMBL, KC_A)
+#define L_SYMSP LT(SYMBL, KC_SPC)
+#define L_BSMSP LT(BARESBL, KC_SPC)
+
+#define L_NUMTB LT(NUMER, KC_TAB)
+#define L_NUMEN LT(NUMER, KC_ENT)
+#define L_NUM_Y LT(NUMER, KC_Y)
+#define L_NUM_W LT(NUMER, KC_W)
+
+#define L_FLRTB LT(FLAYER, KC_TAB)
+
+#define  L_BKLBS  LT(BAREBKL, KC_BSPC)
+#define  L_NUMSP  LT(NUMER, KC_SPC)
+
+/* #define OS_NUM OSL(NUMER) */
+#define T_MOUSE TT(MOUSE)
+
+// Modifier Switching.
+#define  MSFT_E MT(MOD_LSFT, KC_E)
+#define  MSFT_R  MT(MOD_LSFT, KC_R)  // LSFT for keys implementing custom shift values.
+#define  MSFT_EN  MT(MOD_LSFT, KC_ENT)  // LSFT for keys implementing custom shift values.
+#define  MSFT_ES MT(MOD_LSFT, KC_ESC)  // LSFT for keys implementing custom shift values.
+#define  MSFT_UP MT(MOD_LSFT, KC_UP)
+
+#define  MALT_A MT(MOD_LALT, KC_A)
+#define  MALT_Y MT(MOD_LALT, KC_Y)
+#define  MALT_I MT(MOD_LALT, KC_I)
+#define  MALT_SC MT(MOD_RALT, KC_SCLN)
+#define  MALT_W MT(MOD_RALT, KC_W)
+#define  MALT_T MT(MOD_RALT, KC_T)
+
+
+#define  MCTL_L MT(MOD_RCTL, KC_L)
+#define  MCTL_T MT(MOD_RCTL, KC_T)
+#define  MCTL_I MT(MOD_RCTL, KC_I)
+#define  MCTL_A MT(MOD_RCTL, KC_A)
+#define  MCTL_S MT(MOD_LCTL, KC_S)
+// TODO: investigate why HOME_RB and } produse '}', should be nothing?
+#define  HOME_LB MT(MOD_LALT, KC_LBRC)
+#define  HOME_RB MT(MOD_LCTL, KC_RBRC)
+
+#define  MCTL_0 MT(MOD_RCTL, KC_0)
+#define  MSFT_6 MT(MOD_RSFT, KC_6)
+#define  MALT_7 MT(MOD_RALT, KC_7)
+#define  MCTL_1 MT(MOD_LCTL, KC_1)
+#define  MSFT_2 MT(MOD_LSFT, KC_2)
+#define  MALT_3 MT(MOD_LALT, KC_3)
+
+#define  MGUI_ES MT(MOD_LGUI, KC_ESC)
+
+// One shot modifiers.
+#define OS_LGUI OSM(MOD_LGUI)
+#define LM_I3WM LM(I3WM, MOD_LGUI)
+
+// Key code abbreviations.
+#define  KC_RGHT KC_RIGHT
+#define  KC_PGDN KC_PGDOWN
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
-               KC_1, KC_2, KC_3, KC_4, KC_5,    KC_6, KC_7, KC_8, KC_9, KC_0, \
-               KC_A, KC_B, KC_C, KC_D, KC_E,    KC_F, KC_G, KC_H, KC_I, KC_J, \
-               KC_K, KC_L, KC_M, KC_N, KC_O,    KC_P, KC_Q, KC_R, KC_S, KC_T, \
-                           KC_U, KC_V, KC_W,    KC_X, KC_Y, KC_Z              \
-  ),
-};
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QMKBEST:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("QMK is the best thing ever!");
-      } else {
-        // when keycode QMKBEST is released
-      }
-      break;
-    case QMKURL:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKURL is released
-      }
-      break;
-  }
-  return true;
-}
+/* _QWERTY
+ * ,----------------------------------.             ,----------------------------------.
+ * |  Q   |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |
+ * |------+------+------+------+------+             +------+------+------+------+------|
+ * |  A   |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |
+ * |------+------+------+------+------+             +------+------+------+------+------|
+ * |  Z   |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |
+ * `------+------+------+------+------+             +------+------+------+------+------'
+ *               | GUI  | Spc  | Tab  |             | BkSp |  Entr | Esc |
+ *               `--------------------'             `--------------------'
+ *                         ^      ^                     ^      ^      ^
+ *                        Symb   Nav                   Shft   Num    Shft
+ */
+[_QWERTY] = LAYOUT(
+  KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   ,
+  MALT_A , MCTL_S , KC_D   , KC_F   , KC_G   ,                      KC_H   , KC_J   , KC_K   , MCTL_L , MALT_SC,
+  KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                      KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,
+                    OS_LGUI, L_SYMSP, L_NAVBS,                      L_NUMTB, MSFT_EN, MSFT_ES
+),
+
+};
 
 void matrix_init_user(void) {
 
