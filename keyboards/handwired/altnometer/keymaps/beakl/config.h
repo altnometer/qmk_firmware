6136e9f10978988a36599db91f82d7aac14d98fc
sam at lf <sam\@lf>
2020-02-28 11:52:27 +0300

    handwired: add beakl keymap files

---
+++ keyboards/handwired/altnometer/keymaps/beakl/config.h
@@ -0,0 +1,25 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+*/
+
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 1
+/* #define TAPPING_TERM 200 */
+#define TAPPING_TERM 170
+/* #define ONESHOT_TAP_TOGGLE 3 */
+/* #define ONESHOT_LAYER_TOGGLE 3 */
+/* #define ONESHOT_TIMEOUT 800 */
+
+#define PREVENT_STUCK_MODIFIERS
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+
+#endif
